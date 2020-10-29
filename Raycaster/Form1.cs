8d195415c0d67088165ec3a46ca78269b9e01beb
Mikah Kainen <mikah@mltd.com>
2020-10-29 14:45:18 -0700

    Made disco ball

--- Raycaster/Form1.cs
+++ Raycaster/Form1.cs
@@ -16,6 +16,7 @@ namespace Raycaster
         Bitmap canvas;
 
         Point cursorLoc => pictureBox1.PointToClient(MousePosition);
+        
         public Form1()
         {
             InitializeComponent();
@@ -27,33 +28,67 @@ namespace Raycaster
             gfx = Graphics.FromImage(canvas);
         }
 
-        public int returnCircle(int x)
+        public void DrawCircle(int x, int y, int radius)
         {
-            return 
+            for(int i = 0; i < 120; i ++)
+            {
+                gfx.DrawLine(Pens.White, x, y, radius*(float)Math.Cos(3*i) + x, radius*(float)Math.Sin(3*i) + y);
 
-                //return equation of circle translated up to cursor mark
+                //gfx.DrawLine(Pens.White, x, y, i + y, returnCircle(i, radius) + y);
+                //gfx.DrawLine(Pens.White, x, y, i, -returnCircle(i, radius));
+            }
+            //return equation of circle translated up to cursor mark
+        }
+
+        public List<List<int>> DrawRandom()
+        {
+            Random random = new Random();
+            //int[][] returnArray = new int[4][4];
+            List<List<int>> returnArray = new List<List<int>>();
+            for(int i = 0; i < 4; i ++)
+            {
+                returnArray.Add(new List<int>());
+                for(int x = 0; x < 4; x ++)
+                {
+                    returnArray[i].Add(0);
+                }
+            }
+
+            for(int i = 0; i < 4; i ++)
+            {
+                for(int x = 0; x < 2; x ++)
+                {
+                    returnArray[i][2*x] = random.Next(0, 801);
+                }
+                for(int y = 0; y < 2; y ++)
+                {
+                    returnArray[i][2*y + 1] = random.Next(0, 451);
+                }
+                gfx.DrawLine(Pens.White, returnArray[i][0], returnArray[i][1], returnArray[i][2], returnArray[i][3]);
+            }
+            return returnArray;
+        }
+
+        public int returnCircle(int x, int radius)
+        {
+            return (int)Math.Sqrt(radius * radius - x * x);
         }
 
         //https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
         private void timer1_Tick(object sender, EventArgs e)
         {
             gfx.Clear(Color.Black);
-
             // drawing and update happens here
 
-            for(int i = 0; i < 10; i ++)
-            {
-                gfx.DrawLine(Pens.White, cursorLoc.X - 10, cursorLoc.Y - 10 + i, cursorLoc.X + 10, cursorLoc.Y + 10 + i);
-            }
+            DrawCircle(cursorLoc.X, cursorLoc.Y, 20);
 
             // 0,0 is top left, x increases to the right, y increases towards the bottom
             this.Text = cursorLoc.ToString();   // set title of the form
             gfx.DrawLine(Pens.White, 0, 0, 100, 100);
 
-
+            DrawRandom();
             // this should be last line ideally to show the stuff we drew 
             pictureBox1.Image = canvas;
-
         }
     }
 }
